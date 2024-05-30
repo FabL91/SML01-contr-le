375d1a3d78eff837f838934190331c79de37af1e
LEPETIT Fabien <flepetit@extra.cea.fr>
2024-05-30 14:59:26 +0200

    1er dépot

---
+++ Hello.py
@@ -0,0 +1,13 @@
+
+"""Petit code d'essai"""
+import random
+ 
+alist = []
+alist.append(4)
+alist.append(55.89)
+alist.append('a short string')
+alist.append(random.random)
+
+print (alist[1])
+
+print("j'adore python")
---
+++ SML01_connect.py
@@ -0,0 +1,19 @@
+from RsInstrument import *
+
+instr = RsInstrument('ASRL4::INSTR', id_query=True, reset=False)
+idn = instr.query_str('*IDN?')
+print('Hello, I am: ' + idn)
+
+#fréquence d'utilisation du générateur
+freq = instr.query_str('FREQuency?')
+print("Fréquence d'utilisation: " + freq)
+
+instr.write_str('FREQ 83MHz')
+
+instr.write_str('POW -1.6dBm')
+
+#Set RF output ON/OFF
+instr.write_str('OUTP:STAT ON')
+
+# Close the session
+instr.close()
