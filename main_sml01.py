6a710569ac2b48626dd08d2927c329abe0fef9c2
FabL91 <fabien.lepetit@cea.fr>
2024-06-03 11:43:54 +0200

    mise à jour

--- main_sml01.py
+++ main_sml01.py
@@ -23,16 +23,27 @@ class MainWindow(QMainWindow):
         # Créez un dictionnaire vide pour stocker les valeurs sélectionnées
         self.selected_values = {}
 
+        self.ui.comboBox_PortDetect.addItems(visa_list)
+
         # Ajoutez un gestionnaire de signal pour la combobox
         self.ui.comboBox_PortDetect.currentIndexChanged.connect(self.handle_combobox_change)
         
         # Ajoutez des éléments à la combobox (remplacez par vos valeurs)
-        self.ui.comboBox_PortDetect.addItems(["Salut", "Hi", "Hej", "Hallo", "Saluton"])
-
-        #self.ui.comboBox_PortDetect.addItems(visa_list) 
+        #self.ui.comboBox_PortDetect.addItems(["Salut", "Hi", "Hej", "Hallo", "Saluton"])
+        
+        # Chargez les données à partir du fichier "donnees.txt" dans la combobox
+        try:
+            with open("donnees.txt", "r") as fichier:
+                json_data = fichier.read()
+                self.selected_values = json.loads(json_data)
+                print(self.selected_values)
+                
+                self.selected_values["combobox_value"] = self.ui.comboBox_PortDetect.currentText()
+        
+        except FileNotFoundError:
+            print("Le fichier 'donnees.txt' n'a pas été trouvé.")
 
     def handle_combobox_change(self, index):
-        # Récupérez la valeur sélectionnée dans la combobox
         selected_value = self.ui.comboBox_PortDetect.currentText()
         
         # Mettez à jour le dictionnaire avec la nouvelle valeur
@@ -40,6 +51,11 @@ class MainWindow(QMainWindow):
         
         # Convertissez le dictionnaire en format JSON
         json_data = json.dumps(self.selected_values)
+        
+        # Enregistrez le JSON dans un fichier texte
+        with open("donnees.txt", "w") as fichier:
+            fichier.write(json_data)
+        
         print(json_data)  # Affichez le JSON (vous pouvez l'enregistrer dans un fichier si nécessaire)
 
 
